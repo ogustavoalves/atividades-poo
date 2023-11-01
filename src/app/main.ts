10190149dffb9fddef5c76f0dfeb16ca5e1ab2b1
Gustavo <gustavodossantos1704@gmail.com>
2023-10-31 22:21:56 -0300

    :construction: cadastro produto edicao cliente

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,9 @@ import Entrada from "../io/entrada";
 import Empresa from "../modelo/empresa";
 import CadastroCliente from "../negocio/cadastroCliente";
 import ListagemClientes from "../negocio/listagemClientes";
+import Selecionador from "../negocio/selecionador";
+import EditorCliente from "../negocio/editorCliente";
+import CadastroProduto from "../negocio/cadastroProduto";
 
 console.log(`Bem-vindo ao melhor sistema de gerenciamento de pet shops e clínicas veterinarias`)
 let empresa = new Empresa()
@@ -11,6 +14,8 @@ while (execucao) {
     console.log(`Opções:`);
     console.log(`1 - Cadastrar cliente`);
     console.log(`2 - Listar todos os clientes`);
+    console.log(`3 - Editar Cliente`)
+    console.log(`4 - Cadastrar novo produto`)
     console.log(`0 - Sair`);
 
     let entrada = new Entrada()
@@ -24,6 +29,18 @@ while (execucao) {
         case 2:
             let listagem = new ListagemClientes(empresa.getClientes)
             listagem.listar()
+            break;
+        case 3:
+            let cpfEditar = entrada.receberTexto('Digite um cpf para edição: ')
+            let selecionadorClienteEditar = new Selecionador (empresa.getClientes)
+            let clienteEditar = selecionadorClienteEditar.selecionar(cpfEditar)
+
+            let editor = new EditorCliente()
+            editor.editar(clienteEditar)
+            break;
+        case 4:
+            let cadastroProdutos = new CadastroProduto(empresa.getProdutos);
+            cadastroProdutos.cadastrar();
             break;
         case 0:
             execucao = false
