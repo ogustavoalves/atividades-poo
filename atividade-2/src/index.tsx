1dc27b431e644559d342df91fba0f545b98253e4
Gustavo <gustavodossantos1704@gmail.com>
2023-11-06 20:42:02 -0300

    add arquivos base

--- atividade-2/src/index.tsx
+++ atividade-2/src/index.tsx
@@ -1,30 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
-import ListaClientes from "./pages/ListaClientes";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import ListaProdutos from './pages/ListaProdutos';
-import ListaPets from './pages/ListaPets';
-
-
+import reportWebVitals from './reportWebVitals';
+import Roteador from './componentes/roteador';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-            <Routes>
-                <Route element={ <App /> }>
-                    <Route index element={ <ListaClientes /> } />
-                    <Route path='/produtos' element={ <ListaProdutos /> } />
-                    <Route path='/pets' element={ <ListaPets /> } />
-                </Route>
-            </Routes>
-      </BrowserRouter>
+    <Roteador />
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
